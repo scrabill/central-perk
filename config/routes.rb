8438fc95b19317f568f4152f8b187e75a1d09f05
scrabill <shannon.crabill@gmail.com>
2019-12-03 21:24:23 -0500

    Adds nested routes for users and orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :menu_items
   resources :order_items
-  resources :orders
-  resources :users
+
+  resources :users do
+    resources :orders
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
