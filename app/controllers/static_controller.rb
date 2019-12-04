cf6c045df57d08c70aeebaa7b81795f8ec69c84c
scrabill <shannon.crabill@gmail.com>
2019-12-04 17:42:59 -0500

    Adds show route

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,31 @@
 class StaticController < ApplicationController
 
-  def home
+  def index
+
   end
-  
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
