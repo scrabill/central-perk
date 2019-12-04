481ff3a74b98058a4f38a7d1a148c2f91da2c755
scrabill <shannon.crabill@gmail.com>
2019-12-03 21:48:49 -0500

    Adds menu_item actions and show view

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,2 +1,30 @@
 class MenuItemsController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+    @item = MenuItem.find_by_id(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
