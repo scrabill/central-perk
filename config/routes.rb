2f78542def6b5751caa4731d46b86cf7fb6bbb0e
scrabill <shannon.crabill@gmail.com>
2019-12-03 21:31:47 -0500

    Adds static controller, route and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :menu_items
   resources :order_items
 
   resources :users do
     resources :orders
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static#home'
 end
