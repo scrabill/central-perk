226b51f4347d2d7a240936c0192c93335c135099
scrabill <shannon.crabill@gmail.com>
2019-12-03 21:19:59 -0500

    Adds User relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :orders
+  has_many :menu_items through: :orders
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true
+
+
 end
