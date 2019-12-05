9950d3f700dcb66532df96829afe477341795709
scrabill <shannon.crabill@gmail.com>
2019-12-05 16:11:07 -0500

    Adds orders#index action and deletes order_item controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,31 @@
 class OrdersController < ApplicationController
+
+  def index
+    @orders = Order.all
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
