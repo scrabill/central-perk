8defa3f6da9e6fdfc23d16a16c20e8ecdf64130d
scrabill <shannon.crabill@gmail.com>
2019-12-09 19:56:08 -0500

    Adds current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  # Will this account for if it is unable to find a current user (no one is logged in)?
+  def current_user
+    @user ||= User.find_by_id(session[:user_id])
+  end
+  
 end
