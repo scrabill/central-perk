ffc12abaf9474018fede53d629debe1c300e2b91
scrabill <shannon.crabill@gmail.com>
2019-12-12 16:39:28 -0500

    Error messages for user signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    redirect_to user_path(@user)
+
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def edit
