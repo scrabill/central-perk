afa56329bfbe145b1b1b53b2f9bdb598a0c4518c
scrabill <shannon.crabill@gmail.com>
2019-12-14 23:25:24 -0500

    Instructions on creating login for Gunther

--- README.md
+++ README.md
@@ -11,20 +11,28 @@ A coffee shop, point-of-sale application build with Ruby on Rails.
 1. Run `bundle install`
 1. Run `rake db:migrate`
 1. Run `rails s` to start the server
-1. Run `rake db:seed` to populate the database with orders and menu items
+
+## Seeding the Database
+
+To test out the app using dummy data, follow these steps.
+
+1. In the root level of the directory, create a `.env` file
+1. Add the following, replacing `your_password` with a string. This will be the login for the user account Gunther, which all orders will belong and be nested under.
+
+```
+PASSWORD=your_password
+```
+
+3. Run `rake db:seed` to populate the database with a user (Gunter), orders and menu items.
 
 ## Enable Google Login
 
-To enable oAuth with Google login, create a `.env` file in the root directory.
-
-In `.env` add the following, replacing the string `YOUR_CLIENT_ID` and `YOUR_CLIENT_SECRET` with a your Google Client ID and Secret.
+To enable oAuth with Google login, add the following to the `.env` file, replacing the string `YOUR_CLIENT_ID` and `YOUR_CLIENT_SECRET` with a your Google Client ID and Secret.
 
 ```
 GOOGLE_CLIENT_ID="YOUR_CLIENT_ID"
 GOOGLE_CLIENT_SECRET="YOUR_CLIENT_SECRET"
 ```
-
-Commit .gitignore
 
 Detailed instructions on creating the Google Client keys can be seen here: [Google Authentication Strategy for Rails 5 Application](https://medium.com/@rachel.hawa/google-authentication-strategy-for-rails-5-application-cd37947d2b1b)
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-banana = MenuItem.create(name: "Banana", description: "Organic and fair trade", price: 1)
-coffee = MenuItem.create(name: "Coffee", description: "Dark roast", price: 5)
-scone = MenuItem.create(name: "Scone", description: "Blueberry Lemon", price: 3)
+
+# Users
+User.create(name: "Gunther", email: "gunther@centralperk.com", password: ENV["PASSWORD"])
 
 # Coffee
-6.times do
-  MenuItem.create(name: Faker::Coffee.unique.blend_name, description: Faker::Coffee.unique.origin + Faker::Coffee.notes)
-end
+# 6.times do
+#   MenuItem.create(name: Faker::Coffee.unique.blend_name, description: Faker::Coffee.unique.origin + Faker::Coffee.notes)
+# end
 
 # Specialty Coffee
 MenuItem.create(name: "Cappuccino", description: "House blend coffee served with steamed and frothed milk.", price: 3.5 )
@@ -23,3 +23,17 @@ MenuItem.create(name: "Macchiato", description: "Espresso with a dash of frothy 
 MenuItem.create(name: "Matcha green tea latte", description: "Matcha tea steeped in steamed milk and lightly sweetened.", price: 3.50)
 MenuItem.create(name: " Chai latte", description: "Black tea steeped in spices including cardamon, star anise, cinnamon and cloves.", price: 2.75)
 MenuItem.create(name: " Assorted teas", description: "Choose from black tea, chamomile or earl grey", price: 1.75)
+
+# Other
+MenuItem.create(name: "Banana", description: "Organic.", price: 1.00 )
+MenuItem.create(name: "Scone", description: "Blueberry, lemon with a royal icing drizzle.", price: 2.0 )
+MenuItem.create(name: "Biscotti", description: "Twice baked and very crispy. Choose from dark chocolate or salted caramel.", price: 1.50)
+
+# => Order(id: integer, user_id: integer, name_for_pickup: string, completed: boolean, total: integer, created_at: datetime, updated_at: datetime
+
+# Parameters: {"utf8"=>"✓", "authenticity_token"=>"lirJJFtQZyajxqNbmF+CaeKvxmvs+FWbg4rgoBSNI4Izgvnm9aXy3hMIlnTW2qhlOGyaQe6JWiTm/W8rFYoY8Q==", "o
+# rder"=>{"user_id"=>"4", "name_for_pickup"=>"", "menu_item_ids"=>["", "35"]}, "commit"=>"Create Order"}
+# Orders
+6.times do
+  order = Order.create!(user_id: 1, name_for_pickup: Faker::TvShows::Friends.character, completed: false, menu_item_ids: [Faker::Number.within(range: 1..9), Faker::Number.within(range: 1..9), Faker::Number.within(range: 1..9)])
+end
