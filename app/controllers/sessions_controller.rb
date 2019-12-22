b0066173940dbd55a0e09570aa9fca15f2076a64
scrabill <shannon.crabill@gmail.com>
2019-12-21 22:12:14 -0500

    Redirect to root after successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+
     @user = User.find_by(email: params[:user][:email])
 
     if @user
       if @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
-        redirect_to user_path(@user)
+        redirect_to root_path
       end
     else
       redirect_to login_path, alert: "User not found. Please try again."
