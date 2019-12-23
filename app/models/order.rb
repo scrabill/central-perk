23f186c054d2947aa89e9083e1a931b8ca6aacd8
scrabill <shannon.crabill@gmail.com>
2019-12-22 20:18:52 -0500

    Adds methods for total order, completed order and most popular items

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,6 +10,36 @@ class Order < ApplicationRecord
 
   scope :completed, -> { where(completed: true) }
 
+  def self.completed_count
+    Order.where(completed: true).count
+  end
+
+  def self.orders_count
+    Order.all.count
+  end
+
+  def self.most_popular
+
+    # TODO: Update to Ruby 2.7 + and use .tally
+    # Get all menu items from all orders by menu_item_id
+
+    all_items = []
+    Order.all.each do |order|
+      all_items << order.menu_item_ids
+    end
+    # Retuurn the array and flatten it
+    all_items.flatten
+
+    # Create a empty hash. For each item in the all_items array, add it as a key in the array. Each time an array item appears, add one to the value for that key {Coffee > 10}
+    freq = all_items.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+
+    # Put in order by value, reverse it and return the first item
+    freq.sort_by{|k,v| v}.reverse.flatten.first
+
+    #Find the most popular item
+    MenuItem.find_by_id(freq.sort_by{|k,v| v}.reverse.flatten.first)
+  end
+
   def time_taken
     created_at.strftime("%A, %d %b %Y %l:%M %p")
   end
