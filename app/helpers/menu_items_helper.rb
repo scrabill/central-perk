3cd4586aaa7dd73821cb4bfe2c5b21e2fc61d678
scrabill <shannon.crabill@gmail.com>
2019-12-22 21:51:29 -0500

    Helper method to add popular item flag for menu item

--- app/helpers/menu_items_helper.rb
+++ app/helpers/menu_items_helper.rb
@@ -1,2 +1,8 @@
 module MenuItemsHelper
+
+  def is_popular?(item)
+    if item.id == Order.most_popular.id
+      content_tag(:span, 'Popular Item!', class: "tag is-primary is-light is-rounded is-normal")
+    end
+  end
 end
